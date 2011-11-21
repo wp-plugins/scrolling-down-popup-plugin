2e82d4901173ceb5a9c674889f8cf0d52777a035
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-21 15:11:42 +0000

    git-svn-id: https://plugins.svn.wordpress.org/scrolling-down-popup-plugin/trunk@465696 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ help.php
@@ -0,0 +1,11 @@
+<h2>Plugin configuration</h2>
+<ul>
+	<li><a href="http://www.gopiplus.com/work/2011/07/23/scrolling-down-popup-wordpress-plugin/" target="_blank">Add the popup window to particular page using short code.</a></li>
+	<li><a href="http://www.gopiplus.com/work/2011/07/23/scrolling-down-popup-wordpress-plugin/" target="_blank">Add the popup window to all the pages in the website.</a></li>
+</ul>
+<h2>Plugin help</h2>
+<ul>
+	<li>Plugin created by <a href="http://www.gopiplus.com/work/2011/07/23/scrolling-down-popup-wordpress-plugin/" target="_blank">www.gopiplus.com</a></li>
+    <li><a href="http://www.gopiplus.com/work/2011/07/23/scrolling-down-popup-wordpress-plugin/" target="_blank">Click here for more help.</a></li>
+    <li><a href="http://www.gopiplus.com/work/2011/07/23/scrolling-down-popup-wordpress-plugin/" target="_blank">Click here for live demo.</a></li>
+</ul>
---
+++ setting.js
@@ -0,0 +1,66 @@
+/**
+ *     Scrolling down popup plugin
+ *     Copyright (C) 2011  www.gopiplus.com
+ * 
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+function _sdp_submit()
+{
+	if((document.sdp_form.sdp_width.value=="") || isNaN(document.sdp_form.sdp_width.value))
+	{
+		alert("Please enter the width, only number.")
+		document.sdp_form.sdp_width.focus();
+		return false;
+	}
+	else if((document.sdp_form.sdp_left_space.value=="") || isNaN(document.sdp_form.sdp_left_space.value))
+	{
+		alert("Please enter the position (left space), only number.")
+		document.sdp_form.sdp_left_space.focus();
+		return false;
+	}
+	else if((document.sdp_form.sdp_top_space.value=="") || isNaN(document.sdp_form.sdp_top_space.value))
+	{
+		alert("Please enter the position (top space), only number.")
+		document.sdp_form.sdp_top_space.focus();
+		return false;
+	}
+	else if((document.sdp_form.sdp_speed.value=="") || isNaN(document.sdp_form.sdp_speed.value))
+	{
+		alert("Please enter the scrolling speed, only number.")
+		document.sdp_form.sdp_speed.focus();
+		return false;
+	}
+	else if(document.sdp_form.sdp_text.value=="")
+	{
+		alert("Please enter the message.")
+		document.sdp_form.sdp_text.focus();
+		return false;
+	}
+
+}
+
+function _sdp_delete(id)
+{
+	if(confirm("Do you want to delete this record?"))
+	{
+		document.sdp_display.action="options-general.php?page=scrolling-down-popup-plugin/content-management.php&AC=DEL&DID="+id;
+		document.sdp_display.submit();
+	}
+}	
+
+function _sdp_redirect()
+{
+	window.location = "options-general.php?page=scrolling-down-popup-plugin/content-management.php";
+}
