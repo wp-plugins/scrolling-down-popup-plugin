f97486326c8da3ddadaeded761c151b1c1a96322
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-21 14:56:03 +0000

    git-svn-id: https://plugins.svn.wordpress.org/scrolling-down-popup-plugin/tags/1.0@465680 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ button.php
@@ -0,0 +1,4 @@
+<div style="float:right;">
+  <input name="text_management" lang="text_management" class="button-primary" onClick="location.href='options-general.php?page=scrolling-down-popup-plugin/content-management.php'" value="Go to - Content Management" type="button" />
+  <input name="setting_management" lang="setting_management" class="button-primary" onClick="location.href='options-general.php?page=scrolling-down-popup-plugin/scrolling-down-popup-plugin.php'" value="Go to - Popup Setting" type="button" />
+</div>
