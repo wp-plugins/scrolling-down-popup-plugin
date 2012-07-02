081cbcd1838681c346299b7f5d10a9c23ba169a0
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-02 05:06:47 +0000

    git-svn-id: https://plugins.svn.wordpress.org/scrolling-down-popup-plugin/trunk@566286 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- help.php
+++
@@ -1,6 +0,0 @@
-<h2>Plugin configuration option</h2>
-<ol>
-	<li>Add the popup window to particular page using short code.</li>
-	<li>Add the popup window to all the pages in the website.</li>
-</ol>
-Check official website for more info <a href="http://www.gopiplus.com/work/2011/07/23/scrolling-down-popup-wordpress-plugin/" target="_blank">click here</a>
